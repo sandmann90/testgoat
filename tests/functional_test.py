e4776b5f6f54847281ae8b16b58421ba96095dc4
sandmann90 <mair@zsi.at>
2017-11-02 18:29:52 +0100

    initial commit

---
+++ tests/functional_test.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get('http://localhost:8000')
+assert 'Django' in driver.title
+
+
